71e742649f9b85aab0ace6f95624045d74b6bf6d
swsoyee <swsoyee@gmail.com>
2019-01-09 21:16:20 +0900

    Update psnineplus.js

--- psnineplus.js
+++ psnineplus.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PSN中文网功能增强
 // @namespace    https://swsoyee.github.io
-// @version      0.63
+// @version      0.64
 // @description  数折价格走势图，显示人民币价格，奖杯统计，发帖字数统计，楼主高亮，屏蔽黑名单用户发言，被@用户的发言内容显示等多项功能优化P9体验
 // @icon         data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAMAAAAp4XiDAAAAMFBMVEVHcEw0mNs0mNs0mNs0mNs0mNs0mNs0mNs0mNs0mNs0mNs0mNs0mNs0mNs0mNs0mNuEOyNSAAAAD3RSTlMAQMAQ4PCApCBQcDBg0JD74B98AAABN0lEQVRIx+2WQRaDIAxECSACWLn/bdsCIkNQ2XXT2bTyHEx+glGIv4STU3KNRccp6dNh4qTM4VDLrGVRxbLGaa3ZQSVQulVJl5JFlh3cLdNyk/xe2IXz4DqYLhZ4mWtHd4/SLY/QQwKmWmGcmUfHb4O1mu8BIPGw4Hg1TEvySQGWoBcItgxndmsbhtJd6baukIKnt525W4anygNECVc1UD8uVbRNbumZNl6UmkagHeRJfX0BdM5NXgA+ZKESpiJ9tRFftZEvue2cS6cKOrGk/IOLTLUcaXuZHrZDq3FB2IonOBCHIy8Bs1Zzo1MxVH+m8fQ+nFeCQM3MWwEsWsy8e8Di7meA5Bb5MDYCt4SnUbP3lv1xOuWuOi3j5kJ5tPiZKahbi54anNRaaG7YElFKQBHR/9PjN3oD6fkt9WKF9rgAAAAASUVORK5CYII=
 // @author       InfinityLoop, mordom0404
@@ -10,7 +10,6 @@
 // @require      http://cdn.staticfile.org/jquery/2.1.4/jquery.min.js
 // @require      http://code.highcharts.com/highcharts.js
 // @require      https://unpkg.com/tippy.js@3/dist/tippy.all.min.js
-// @require      https://cdn.jsdelivr.net/npm/js-bbcode-parser@2.0.0/parser.min.js
 // @license      MIT
 // @supportURL   https://github.com/swsoyee/psnine-night-mode-CSS/issues/new
 // @compatible   chrome
@@ -27,6 +26,8 @@
         addNews: false, // 设置为false默认关闭
         // 功能0-3设置：鼠标滑过黑条即可显示内容
 		hoverUnmark: true, // 设置为false则选中才显示
+        // 功能0-5设置：是否开启自动签到
+        autoCheckIn: true,
 		//功能1-4：回复内容回溯
 		replyTraceback: true,
         // 功能1-1设置：高亮发帖楼主功能
@@ -106,6 +107,11 @@
         })
     }
     // 功能0-4：markdown语法支持测试
+
+    // 功能0-5：自动签到
+    if( $("[class$=yuan]").length > 0 ) {
+        $("[class$=yuan]").click()
+    }
 
     // 帖子优化
     // 功能1-1：高亮发帖楼主
