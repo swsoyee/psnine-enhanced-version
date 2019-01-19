7122ff9948d7fa04860cf0c9414f109c5cd5167f
swsoyee <swsoyee@gmail.com>
2019-01-19 20:42:30 +0900

    个人主页下显示游戏自动翻页

--- psnineplus.js
+++ psnineplus.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PSN中文网功能增强
 // @namespace    https://swsoyee.github.io
-// @version      0.73
+// @version      0.74
 // @description  数折价格走势图，显示人民币价格，奖杯统计和筛选，发帖字数统计和即时预览，楼主高亮，自动翻页，屏蔽黑名单用户发言，被@用户的发言内容显示等多项功能优化P9体验
 // @icon         data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAMAAAAp4XiDAAAAMFBMVEVHcEw0mNs0mNs0mNs0mNs0mNs0mNs0mNs0mNs0mNs0mNs0mNs0mNs0mNs0mNs0mNuEOyNSAAAAD3RSTlMAQMAQ4PCApCBQcDBg0JD74B98AAABN0lEQVRIx+2WQRaDIAxECSACWLn/bdsCIkNQ2XXT2bTyHEx+glGIv4STU3KNRccp6dNh4qTM4VDLrGVRxbLGaa3ZQSVQulVJl5JFlh3cLdNyk/xe2IXz4DqYLhZ4mWtHd4/SLY/QQwKmWmGcmUfHb4O1mu8BIPGw4Hg1TEvySQGWoBcItgxndmsbhtJd6baukIKnt525W4anygNECVc1UD8uVbRNbumZNl6UmkagHeRJfX0BdM5NXgA+ZKESpiJ9tRFftZEvue2cS6cKOrGk/IOLTLUcaXuZHrZDq3FB2IonOBCHIy8Bs1Zzo1MxVH+m8fQ+nFeCQM3MWwEsWsy8e8Di7meA5Bb5MDYCt4SnUbP3lv1xOuWuOi3j5kJ5tPiZKahbi54anNRaaG7YElFKQBHR/9PjN3oD6fkt9WKF9rgAAAAASUVORK5CYII=
 // @author       InfinityLoop, mordom0404
@@ -121,9 +121,33 @@
 
     // 功能0-6：自动翻页
     if(settings.autoPaging) {
+        var isbool = true; //触发开关，防止多次调用事件
+        $("body").append("<div id='loadingMessage' style='position: absolute;bottom: 0px;position: fixed;right: 1px !important;display:none; color:white;'></div>")
+        if(/psnid\/[A-Za-z0-9]+$/.test(window.location.href)) {
+            var gamePageIndex = 2
+            $(window).scroll(function() {
+                if ($(this).scrollTop() + $(window).height() + 700 >= $(document).height() && $(this).scrollTop() > 700 && isbool == true) {
+                    isbool = false;
+                    var gamePage = window.location.href + "/psngame?page=" + gamePageIndex
+                    // 加载页面并且插入
+                    $("#loadingMessage").text("加载第" + gamePageIndex + "页...")
+                    $("#loadingMessage").show()
+                    $.get(gamePage, {}, function(data) {
+                        var $response = $('<div />').html(data);
+                        var nextGameContent = $response.find('tbody > tr')
+                        if(nextGameContent.length > 0) {
+                            $("tbody > tr:last").after(nextGameContent);
+                            isbool = true;
+                            gamePageIndex += 1
+                        } else {
+                            $("#loadingMessage").text("没有更多游戏了...")
+                        }
+                    },'html');
+                    setTimeout(function(){$("#loadingMessage").fadeOut();},2000);
+                }
+            })
+        }
         if(/((gene($|\?))|(qa($|\?))|(topic($|\?))|(trade($|\?)))/.test(window.location.href)) {
-            var isbool = true; //触发开关，防止多次调用事件
-            $("body").append("<div id='loadingMessage' style='position: absolute;bottom: 0px;position: fixed;right: 1px !important;display:none; color:white;'></div>")
             $(window).scroll(function() {
                 //当内容滚动到底部时加载新的内容
                 if ($(this).scrollTop() + $(window).height() + 700 >= $(document).height() && $(this).scrollTop() > 700 && isbool == true) {
