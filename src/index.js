cf3b5145d66fd200179b7dd32d73e2de0fbe03b2
swsoyee <swsoyee@gmail.com>
2020-10-03 15:46:45 +0900

    add: hotTag function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 GM_addStyle(require('./resource/style/icon.css').toString());
 
 import pageBottom from './components/PageBottomButton';
+import hotTag from './components/HotTag';
 import beautifyQaIndex from './components/BeautifyQaIndex';
 import showMarkMessage from './components/ShowMarkMessage';
 import OpBadge from './components/OpBadge';
@@ -9,6 +10,7 @@ const page = window.location.href;
 
 // 全局
 pageBottom();
+hotTag(30); // TODO 设置面板
 showMarkMessage(true); // TODO 设置面板
 
 // TODO Router refactor
