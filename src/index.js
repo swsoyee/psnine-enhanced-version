fb5164aaae475951a635e107676caf7ff25a3f5d
swsoyee <swsoyee@gmail.com>
2020-10-03 14:55:00 +0900

    add: showMarkMessage function

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ GM_addStyle(require('./resource/style/icon.css').toString());
 
 import pageBottom from './components/PageBottomButton';
 import beautifyQaIndex from './components/BeautifyQaIndex';
+import showMarkMessage from './components/showMarkMessage';
 
 const page = window.location.href;
+
+// 全局
+pageBottom();
+showMarkMessage(true); // TODO 设置面板
 
 // 导航 > 问答
 if (/\/qa/.test(page)) {
     beautifyQaIndex(true); // TODO 设置面板
 }
 
-pageBottom();
