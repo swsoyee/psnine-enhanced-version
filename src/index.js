a3ea5522724e6a4c1b8e57dde2164c651634ea3a
swsoyee <swsoyee@gmail.com>
2020-10-03 14:42:48 +0900

    add: beautifyQaIndex function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-import PageBottom from './components/PageBottomButton/PageBottomButton';
+GM_addStyle(require('./resource/style/icon.css').toString());
 
-PageBottom();
+import pageBottom from './components/PageBottomButton';
+import beautifyQaIndex from './components/BeautifyQaIndex';
+
+const page = window.location.href;
+
+// 导航 > 问答
+if (/\/qa/.test(page)) {
+    beautifyQaIndex(true); // TODO 设置面板
+}
+
+pageBottom();
