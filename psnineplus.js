371bcf3285077fbd2fd610b0280f4f9d3e2c493b
swsoyee <swsoyee@gmail.com>
2020-11-06 14:18:37 +0900

    fix(自动翻页): 修复设置失效问题
    在 https://github.com/swsoyee/psnine-enhanced-version/pull/62/files#diff-cebc31b81dfd21506cb081d3c38f7e1045e41b3cbc2897f7d8c003bd6de5c108L2552 中该设置选项被误删除

--- psnineplus.js
+++ psnineplus.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PSN中文网功能增强
 // @namespace    https://swsoyee.github.io
-// @version      0.9.41
+// @version      0.9.42
 // @description  数折价格走势图，显示人民币价格，奖杯统计和筛选，发帖字数统计和即时预览，楼主高亮，自动翻页，屏蔽黑名单用户发言，被@用户的发言内容显示等多项功能优化P9体验
 // @icon         data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAMAAAAp4XiDAAAAMFBMVEVHcEw0mNs0mNs0mNs0mNs0mNs0mNs0mNs0mNs0mNs0mNs0mNs0mNs0mNs0mNs0mNuEOyNSAAAAD3RSTlMAQMAQ4PCApCBQcDBg0JD74B98AAABN0lEQVRIx+2WQRaDIAxECSACWLn/bdsCIkNQ2XXT2bTyHEx+glGIv4STU3KNRccp6dNh4qTM4VDLrGVRxbLGaa3ZQSVQulVJl5JFlh3cLdNyk/xe2IXz4DqYLhZ4mWtHd4/SLY/QQwKmWmGcmUfHb4O1mu8BIPGw4Hg1TEvySQGWoBcItgxndmsbhtJd6baukIKnt525W4anygNECVc1UD8uVbRNbumZNl6UmkagHeRJfX0BdM5NXgA+ZKESpiJ9tRFftZEvue2cS6cKOrGk/IOLTLUcaXuZHrZDq3FB2IonOBCHIy8Bs1Zzo1MxVH+m8fQ+nFeCQM3MWwEsWsy8e8Di7meA5Bb5MDYCt4SnUbP3lv1xOuWuOi3j5kJ5tPiZKahbi54anNRaaG7YElFKQBHR/9PjN3oD6fkt9WKF9rgAAAAASUVORK5CYII=
 // @author       InfinityLoop, mordom0404, Nathaniel_Wu, JayusTree
@@ -560,7 +560,7 @@
             // 递归获取 DOM 里的纯文本
             const truncateElem = (elem, reqCount) => {
                 let grabText = '', missCount = reqCount;
-                $(elem).contents().each(function() {
+                $(elem).contents().each(function () {
                     switch (this.nodeType) {
                         case Node.TEXT_NODE:
                             // Get node text, limited to missCount.
@@ -581,10 +581,10 @@
                 });
                 return {
                     text:
-                    // Wrap text using current elem tag.
-                    elem.outerHTML.match(/^<[^>]+>/m)[0]
-                    + grabText
-                    + '</' + elem.localName + '>',
+                        // Wrap text using current elem tag.
+                        elem.outerHTML.match(/^<[^>]+>/m)[0]
+                        + grabText
+                        + '</' + elem.localName + '>',
                     count: reqCount - missCount,
                 };
             };
@@ -2789,6 +2789,8 @@
                     $('#hotTagThresholdValue').html(value);
                     newSettings.hotTagThreshold = value;
                 });
+                // 自动翻页的页数设定
+                $('#autoPaging').val(newSettings.autoPaging);
                 // 高亮用户
                 const highlightSpecificIDText = newSettings.highlightSpecificID.length
                     ? newSettings.highlightSpecificID.join(',')
@@ -2831,6 +2833,7 @@
                     : [];
                 newSettings.filterNonePlatinumAlpha = Number($('#filterNonePlatinum').val());
                 newSettings.hotTagThreshold = Number($('#hotTagThreshold').val());
+                newSettings.autoPaging = Number($('#autoPaging').val());
                 $('.setting-panel-box').removeClass('show');
                 localStorage['psnine-night-mode-CSS-settings'] = JSON.stringify(
                     newSettings
