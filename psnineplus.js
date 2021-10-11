2ea70eb6899fb7af260072d6d20a698c7fdb60e1
InfinityLoop <swsoyee@gmail.com>
2021-10-11 22:51:36 +0900

    feat: highlight source reply

--- psnineplus.js
+++ psnineplus.js
@@ -725,6 +725,7 @@
                     const targetTop = replyContentObjectOriginal.get(0).getBoundingClientRect().top;
                     const currentTop = $(this).get(0).getBoundingClientRect().top;
                     window.scrollBy({ top: targetTop - currentTop, behavior: 'smooth' });
+                    $(replyContentObjectOriginal).fadeOut(500).fadeIn(500).fadeOut(500).fadeIn(500);
                   });
                   // 鼠标悬浮变手形样式
                   $(`.responserContent_${floor}_${outputID}`).css('cursor', 'pointer');
