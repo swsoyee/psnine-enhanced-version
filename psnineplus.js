af061ce203a1f0834a540d54a5a61bec1fbc56bd
Tianyu Wu <tianyu_wu@ncsu.edu>
2023-02-26 01:13:10 -0500

    [fix] improved sortTipsByLikes() compatibility with old pages

--- psnineplus.js
+++ psnineplus.js
@@ -2508,78 +2508,78 @@
     };
     referGameVariants(settings.referGameVariants, settings.preferSearchForFindingVariants);
 
-    /*
-         * 功能：奖杯心得按“顶”的数量排序功能
-         */
-    const sortTipsByLikes = (isSorted) => {
-      // 检测是否为老页面
-      const containerName = $('.post').length > 0 ? '.mt20' : '.list';
-      $(containerName).css({
-        display: 'flex',
-        flexDirection: 'column',
-      });
-      // 遍历tips容器下面的每一个子元素
-      $(`${containerName}>*`).each((index, ele) => {
-        // 获取顶元素
-        const $likeEle = $(ele).find('.text-success')[0];
-        let likeStr = '';
-        if ($likeEle) {
-          // 获取顶数
-          likeStr = $likeEle.innerHTML;
-          likeStr = likeStr.replace(/[^0-9]/ig, '');
-        }
-        if (!isSorted) {
-          $(ele).css({
-            order: likeStr ? 99999 - likeStr : 99999,
-          });
+    // 奖杯心得页面加入Tips排序选项、输入框加入可缩放大小
+    const trophyTipsEnhancement = () => {
+      const content = $('div.min-inner.mt40 > div.box.mt20');
+      const oldTips = content.children('div.post');
+      const newTipsContainer = content.children('ul.list');
+      const newTips = newTipsContainer.children('li');
+      if (window.location.href.match(/trophy\/\d+($|\/$)/) && (newTips.length || oldTips.length)) {
+        // 加入Tips排序选项
+        let isSorted = false;
+        const sortButtonHTML = '<div id="sortTipsByLikes">将Tips按热门排序</div>';
+        const tipsContainerStyle = {
+          position: 'relative',
+          display: 'flex',
+          flexDirection: 'column',
+        };
+        let tips;
+        if (newTips.length) {
+          newTipsContainer.before(sortButtonHTML).css(tipsContainerStyle);
+          tips = newTips;
         } else {
-          $(ele).css({
-            order: 0,
+          // 将旧版Tips收入单独的容器以在不影响其他元素的情况下排序
+          $(oldTips[0]).before(sortButtonHTML);
+          $(oldTips[0]).before('<div id="tipsContainer" />');
+          const oldTipsContainer = content.children('#tipsContainer').css(tipsContainerStyle);
+          oldTips.each((i, e) => {
+            $(e).detach();
+            oldTipsContainer.append(e);
           });
+          tips = oldTips;
         }
-      });
-      // 把警告信息和排序按钮写死为第一位
-      $('.alert-error, #sortTipsByLikes').css({
-        order: 0,
-      });
-    };
-
-    // 奖杯心得页面输入框可缩放大小
-    if (window.location.href.match(/trophy\/\d+($|\/$)/) && $('div.box.mt20>.list>li').length) {
-      let isSorted = false;
-      $('div.box.mt20>.list').prepend("<div id='sortTipsByLikes'>将tips按热门排序</div>").css({
-        position: 'relative',
-      });
-      $('#sortTipsByLikes').css({
-        padding: '8px 6px',
-        'font-size': '12px',
-        'text-align': 'center',
-        'background-color': '#3890ff',
-        color: '#FFFFFF',
-        cursor: 'pointer',
-        margin: '10px',
-      })
-        .click((event) => {
+        const sortTipsByLikes = () => {
+          tips.each((index, tip) => {
+            if (!isSorted) {
+              // 获取顶元素
+              const likesText = $(tip).find('.text-success')[0].innerHTML.replace(/[^\d]/g, '');
+              $(tip).css({ order: likesText ? 9999 - likesText : 9999 });
+            } else {
+              $(tip).css({ order: 0 });
+            }
+          });
+        };
+        content.children('#sortTipsByLikes').css({
+          padding: '8px 6px',
+          'font-size': '12px',
+          'text-align': 'center',
+          'background-color': '#3890ff',
+          color: '#FFFFFF',
+          cursor: 'pointer',
+          margin: '10px',
+        }).click((event) => {
+          sortTipsByLikes();
           if (isSorted) {
-            sortTipsByLikes(isSorted);
-            $(event.target).text('将tips按热门排序').css({
+            $(event.target).text('将Tips按热门排序').css({
               'background-color': '#3890ff',
               color: '#FFFFFF',
             });
           } else {
-            sortTipsByLikes(isSorted);
-            $(event.target).text('将tips按时间排序').css({
+            $(event.target).text('将Tips按时间排序').css({
               'background-color': '#E7EBEE',
               color: '#99A1A7',
             });
           }
           isSorted = !isSorted;
         });
-      $('#comment').css({
-        resize: 'vertical',
-        minHeight: 200,
-      });
-    }
+        // 输入框允许大小缩放
+        content.find('#comment').css({
+          resize: 'vertical',
+          minHeight: 200,
+        });
+      }
+    };
+    trophyTipsEnhancement();
 
     // P9时间格式转换函数
     function p9TimeTextParser(timestampText) { // returns UTC time
