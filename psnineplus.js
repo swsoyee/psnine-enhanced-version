ee3f764da2e2fa8035577370ede27b3875cc6122
kaikaiiiiiii <yourkaikai@gmail.com>
2024-07-16 23:41:09 +0800

    clear

--- psnineplus.js
+++ psnineplus.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PSN中文网功能增强
 // @namespace    https://swsoyee.github.io
-// @version      1.0.25
+// @version      1.0.26
 // @description  数折价格走势图，显示人民币价格，奖杯统计和筛选，发帖字数统计和即时预览，楼主高亮，自动翻页，屏蔽黑名单用户发言，被@用户的发言内容显示等多项功能优化P9体验
 // eslint-disable-next-line max-len
 // @icon         data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAMAAAAp4XiDAAAAMFBMVEVHcEw0mNs0mNs0mNs0mNs0mNs0mNs0mNs0mNs0mNs0mNs0mNs0mNs0mNs0mNs0mNuEOyNSAAAAD3RSTlMAQMAQ4PCApCBQcDBg0JD74B98AAABN0lEQVRIx+2WQRaDIAxECSACWLn/bdsCIkNQ2XXT2bTyHEx+glGIv4STU3KNRccp6dNh4qTM4VDLrGVRxbLGaa3ZQSVQulVJl5JFlh3cLdNyk/xe2IXz4DqYLhZ4mWtHd4/SLY/QQwKmWmGcmUfHb4O1mu8BIPGw4Hg1TEvySQGWoBcItgxndmsbhtJd6baukIKnt525W4anygNECVc1UD8uVbRNbumZNl6UmkagHeRJfX0BdM5NXgA+ZKESpiJ9tRFftZEvue2cS6cKOrGk/IOLTLUcaXuZHrZDq3FB2IonOBCHIy8Bs1Zzo1MxVH+m8fQ+nFeCQM3MWwEsWsy8e8Di7meA5Bb5MDYCt4SnUbP3lv1xOuWuOi3j5kJ5tPiZKahbi54anNRaaG7YElFKQBHR/9PjN3oD6fkt9WKF9rgAAAAASUVORK5CYII=
@@ -564,7 +564,7 @@
     let userBattleMonitors = GM_getValue('userBattleMonitors', []);
     let cacheBattleInfo = GM_getValue('cacheBattleInfo', {});
 
-    const updateTopMenuNotice = (lista, listb) => { // 定义函数：添加顶部菜单通知红点
+    const updateTopMenuNotice = (lista, listb) => { // 定义函数：变更顶部菜单通知红点
       let count = 0;
       lista.forEach((item) => {
         if (listb.includes(item)) count += 1;
@@ -609,6 +609,7 @@
       });
     };
 
+    // 页面加载时执行约战监测功能
     if (cacheBattleInfo.lastUpdate && new Date().getTime() - cacheBattleInfo.lastUpdate < settings.BattleInfoUpdateInterval) {
       updateTopMenuNotice(userBattleMonitors, cacheBattleInfo.list);
       console.log('use cached BattleRecuritInfo');
@@ -617,7 +618,7 @@
       console.log('update BattleRecuritInfo');
     }
 
-    // 添加约战监控按钮
+    // 在游戏的约战页面添加约战监控按钮
     if (/\/psngame\/\d+\/battle\/?$/.test(window.location.href)) {
       const gameID = window.location.href.match(/\/psngame\/(\d+)/)[1];
       const actionArea = document.querySelector('center.pd10');
@@ -638,8 +639,7 @@
           flex: 1;
           width: calc(50% - 8px);
           margin: 0 4px;
-        }
-        `;
+        }`;
       document.head.appendChild(style);
 
       // 为 span 元素添加点击事件，切换约战监控状态
